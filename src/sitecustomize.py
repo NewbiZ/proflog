789689d160cc404ca940cf70c03b3a748e1ef642
Aurelien Vallee <aurelien.vallee@protonmail.com>
2025-01-16 15:01:47 +0100

    Gather multilines in a single line

--- src/sitecustomize.py
+++ src/sitecustomize.py
@@ -3,6 +3,8 @@ import traceback
 import os
 import pathlib
 import atexit
+import ast
+import functools
 
 STACKTRACE_EXTRA = bool(os.environ.get("PROFLOG_STACKTRACE_EXTRA", ""))
 STACKTRACE_SIZE = int(os.environ.get("PROFLOG_STACKTRACE_SIZE", "9999"))
@@ -11,6 +13,36 @@ STACKTRACE_FILE = None
 STACKTRACE_PYTHON_PACKAGES = os.environ.get(
     "PROFLOG_STACKTRACE_PYTHON_PACKAGES", ""
 ).split(",")
+
+
+def parse_as_python(s: str):
+    try:
+        ast.parse("async def scope(): " + s)
+    except SyntaxError:
+        return False
+    return True
+
+
+@functools.lru_cache
+def read_file(file: str):
+    with open(file) as f:
+        return [line.strip() for line in f.readlines()]
+
+
+def consolidate_multiline(file: str, lineno: int) -> str:
+    try:
+        lineno -= 1
+        original_lineno = lineno
+        lines = read_file(file)
+        line = lines[lineno]
+        while not parse_as_python(line):
+            lineno += 1
+            if lineno >= len(lines):
+                return lines[original_lineno]
+            line = line + lines[lineno].split("#")[0].strip()
+        return line
+    except:  # noqa
+        return "<unknown>"
 
 
 def dump_stacktrace(signal, frame):
@@ -27,12 +59,13 @@ def dump_stacktrace(signal, frame):
         assert STACKTRACE_FILE
         if STACKTRACE_EXTRA:
             txt = " \x1b[0;34m←\x1b[0m ".join(
-                f"\x1b[0;34m{pathlib.Path(f.filename).name}:{f.lineno}\x1b[0m {f.line or "?"}".strip()
+                f"\x1b[0;34m{pathlib.Path(f.filename).name}:{f.lineno}\x1b[0m {consolidate_multiline(f.filename, f.lineno or 0)}"
                 for f in stacktrace[::-1][:STACKTRACE_SIZE]
             )
         else:
             txt = " \x1b[0;34m←\x1b[0m ".join(
-                (f.line or "").strip() for f in stacktrace[::-1][:STACKTRACE_SIZE]
+                consolidate_multiline(f.filename, f.lineno or 0)
+                for f in stacktrace[::-1][:STACKTRACE_SIZE]
             )
         print(
             txt,
